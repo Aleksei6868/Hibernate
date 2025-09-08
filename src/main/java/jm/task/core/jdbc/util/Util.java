2b4f9bda8e74c5f0d59c8019d358e15f05ab26c8
AAleksei Gorshkov <viking-6868@mail.ru>
2025-09-08 12:59:56 +0300

    I'm redy

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static String URL = "jdbc:mysql://localhost:3306/MySqlConnection";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "viking68";
+
+    public static Connection getConnection () throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
